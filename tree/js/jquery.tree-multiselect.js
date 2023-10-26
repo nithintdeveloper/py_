c1a6fdcefb49913450366d7b7c1a327a80475c6b
nithintdeveloper <127333629+nithintdeveloper@users.noreply.github.com>
2023-10-27 01:02:56 +0530

    Update jquery.tree-multiselect.js

--- tree/js/jquery.tree-multiselect.js
+++ tree/js/jquery.tree-multiselect.js
@@ -1,4 +1,3 @@
-/* jQuery Tree Multiselect v2.6.3 | (c) Patrick Tsai | MIT Licensed */
 (function(){function r(e,n,t){function o(i,f){if(!n[i]){if(!e[i]){var c="function"==typeof require&&require;if(!f&&c)return c(i,!0);if(u)return u(i,!0);var a=new Error("Cannot find module '"+i+"'");throw a.code="MODULE_NOT_FOUND",a}var p=n[i]={exports:{}};e[i][0].call(p.exports,function(r){var n=e[i][1][r];return o(n||r)},p,p.exports,r,e,n,t)}return n[i].exports}for(var u="function"==typeof require&&require,i=0;i<t.length;i++)o(t[i]);return o}return r})()({1:[function(require,module,exports){
 "use strict";
 
@@ -839,17 +838,18 @@ Tree.prototype.render = function (noCallbacks) {
     }
   } // remove items first
 
-
+  var selectedApis = new Set(apis.selectedApis);
   for (var ii = 0; ii < this.keysToRemove.length; ++ii) {
     // remove the selected divs
     var node = this.selectedNodes[this.keysToRemove[ii]];
-
+    //console.log(node);
     if (node) {
+      selectedApis.delete(parseInt(node.getAttribute('data-value')));
+      //console.log(parseInt(node.getAttribute('data-value')));
       // slightly more verbose than node.remove(), but more browser support
       node.parentNode.removeChild(node);
       this.selectedNodes[this.keysToRemove[ii]] = null;
     } // uncheck these checkboxes
-
 
     var selectionNode = this.astItems[this.keysToRemove[ii]].node;
     selectionNode.getElementsByTagName('INPUT')[0].checked = false;
@@ -857,7 +857,6 @@ Tree.prototype.render = function (noCallbacks) {
 
   Util.array.subtract(this.selectedKeys, this.keysToRemove); // now add items
 
-  var selectedApis = new Set(apis.selectedApis);
   for (var jj = 0; jj < this.keysToAdd.length; ++jj) {
     // create selected divs
     var key = this.keysToAdd[jj];
