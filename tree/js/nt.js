b6b1bfd7fb9137f9d551b9fd237e16ad594d235a
nithintdeveloper <127333629+nithintdeveloper@users.noreply.github.com>
2023-10-30 21:03:43 +0530

    Update nt.js

--- tree/js/nt.js
+++ tree/js/nt.js
@@ -41,7 +41,24 @@ var apis = {"predefined": [
                      "family": "Maps",
                      "isDefault": true
                  },
-
+                 {
+                     "apiId": 200,
+                     "api": "API200",
+                     "family": "Other",
+                     "isDefault": true
+                 },
+                 {
+                     "apiId": 201,
+                     "api": "API202",
+                     "family": "Other",
+                     "isDefault": true
+                 },
+                 {
+                     "apiId": 202,
+                     "api": "API202",
+                     "family": "Other",
+                     "isDefault": true
+                 },
 
              ],
                  "selectedApis": [101,104]
@@ -50,9 +67,17 @@ var selectedApiIds = apis.selectedApis; // save these ids in the db
 $(document).ready(function() {
     pageRefresh();
      $('#add_apis').click(function(){
-         selectedApiIds = apis.selectedApis; // update only when the button is clicked
-         pageRefresh();
+         if (selectedApiIds.length < apis.selectedApis.length) {
+            selectedApiIds = apis.selectedApis; // update only when the button is clicked
+            pageRefresh();
+         }
       });
+      $('#remove_apis').click(function(){
+               if (selectedApiIds.length > apis.selectedApis.length) {
+                  selectedApiIds = apis.selectedApis; // update only when the button is clicked
+                  pageRefresh();
+               }
+            });
 
 });
 
@@ -63,10 +88,14 @@ function pageRefresh(){
     $('#rightbox_select').empty()
     $("div.tree-multiselect").remove();
     $.each(apis.predefined, function (i) {
-                   var selected = apis.selectedApis.includes(apis.predefined[i].apiId) ? "selected" : "";
-                   $('#leftbox_select').append("<option value="+apis.predefined[i].apiId+" data-section="+apis.predefined[i].family+" "+selected+" data-index='1'>"+apis.predefined[i].api+"</option>");
+                   //var selected = apis.selectedApis.includes(apis.predefined[i].apiId) ? "selected" : "";
+                   var selected = apis.selectedApis.includes(apis.predefined[i].apiId) ? true : false;
+                   //$('#leftbox_select').append("<option value="+apis.predefined[i].apiId+" data-section="+apis.predefined[i].family+" "+selected+" data-index='1'>"+apis.predefined[i].api+"</option>");
                    if (selected){
-                         $('#rightbox_select').append("<option value="+apis.predefined[i].apiId+" data-section="+apis.predefined[i].family+" selected data-index='1'>"+apis.predefined[i].api+"</option>");
+                         $('#rightbox_select').append("<option value="+apis.predefined[i].apiId+" data-section="+apis.predefined[i].family+"  data-index='1'>"+apis.predefined[i].api+"</option>");
+                   }
+                   else {
+                          $('#leftbox_select').append("<option value="+apis.predefined[i].apiId+" data-section="+apis.predefined[i].family+" data-index='1'>"+apis.predefined[i].api+"</option>");
                    }
       });
      $("#leftbox_select").treeMultiselect({
@@ -77,7 +106,6 @@ function pageRefresh(){
                      startCollapsed: false,
                      hideSidePanel: true
         });
-       //$("#leftbox_select").treeMultiselect({ enableSelectAll: true, sortable: true });
        $("#rightbox_select").treeMultiselect({
                      allowBatchSelection: true,
                      enableSelectAll: true,
